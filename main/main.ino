8507f000d583777973ccf3c78b12dd3764632a6c
Guilherme Ribas Carneiro <guilhermecarneiro@alunos.utfpr.edu.br>
2019-11-17 22:56:19 -0200

    Added structure to check display status

--- main/main.ino
+++ main/main.ino
@@ -1,14 +1,15 @@
 #include <SSD1306.h>
 #include <EEPROM.h>
+#include <SPI.h>
 #include <LoRa.h>
 
 // Pinos do lora
-#define SCK 5   // GPIO5  SCK
-#define MISO 19 // GPIO19 MISO
-#define MOSI 27 // GPIO27 MOSI
-#define SS 18   // GPIO18 CS
-#define RST 14  // GPIO14 RESET
-#define DI00 26 // GPIO26 IRQ(Interrupt Request)
+#define SCK 5   // GPIO5
+#define MISO 19 // GPIO19
+#define MOSI 27 // GPIO27
+#define SS 18   // GPIO18
+#define RST 14  // GPIO14
+#define DI00 26 // GPIO26 (Interrupt Request)
 
 // Frequência de comunicação, 868MHz
 #define BAND 868E6
@@ -27,7 +28,8 @@ SSD1306 display(0x3c, 4, 15);
 #define PIN_LED_OK 13
 #define PIN_LED_ERR 12
 
-void initializeDisplay(){
+void initializeDisplay()
+{
   //O estado do GPIO16 é utilizado para controlar o display
   pinMode(16, OUTPUT);
   //Reseta as configurações do display
@@ -36,21 +38,34 @@ void initializeDisplay(){
   digitalWrite(16, HIGH);
 
   //Configurações do display
-  display.init();
+  bool displayStatus = display.init();
   display.flipScreenVertically();
   display.setContrast(255);
   display.setFont(ArialMT_Plain_10);
   display.setTextAlignment(TEXT_ALIGN_LEFT);
+
+  if (!displayStatus)
+  {
+    display.clear();
+    display.setFont(ArialMT_Plain_24);
+    display.drawString(0, 0, "ALERTA!");
+    display.setFont(ArialMT_Plain_10);
+    display.drawString(0, 30, "Erro ao inicializar o Display!");
+    display.display();
+
+    digitalWrite(PIN_LED_ERR, HIGH);
+
+    while(1);
+  }
 }
 
-void welcomeMessage(){
+void welcomeMessage()
+{
   display.clear();
-  
   display.drawString(0, 0, "Sistema de bombeamento");
   display.drawString(0, 15, "d'água a longa");
   display.drawString(0, 30, "distância");
   display.drawString(0, 50, "Faculdade Guarapuava");
-
   display.display();
 
   delay(6000);
@@ -58,39 +73,49 @@ void welcomeMessage(){
   display.clear();
 }
 
-void monitoringMinLimit(){
-  if (digitalRead(PIN_MIN_LIMIT) == LOW && EEPROM.read(0) != 0) {
+void monitoringMinLimit()
+{
+  if (digitalRead(PIN_MIN_LIMIT) == LOW && EEPROM.read(0) != 0)
+  {
     EEPROM.write(0, 0);
     EEPROM.commit();
-    
+
     Serial.println("Ligar Motor");
+    sendPacket(1);
   }
   
-  if (digitalRead(PIN_MIN_LIMIT) == HIGH && EEPROM.read(0) == 0) {
+  if (digitalRead(PIN_MIN_LIMIT) == HIGH && EEPROM.read(0) == 0)
+  {
     EEPROM.write(0, 1);
     EEPROM.commit();
   }
 }
 
-void monitoringMaxLimit(){
-  if (digitalRead(PIN_MAX_LIMIT) == HIGH && EEPROM.read(1) != 0) {
+void monitoringMaxLimit()
+{
+  if (digitalRead(PIN_MAX_LIMIT) == HIGH && EEPROM.read(1) != 0)
+  {
     EEPROM.write(1, 0);
     EEPROM.commit();
     
     Serial.println("Desligar Motor");
+    sendPacket(0);
   }
   
-  if (digitalRead(PIN_MIN_LIMIT) == LOW && EEPROM.read(1) == 0) {
+  if (digitalRead(PIN_MIN_LIMIT) == LOW && EEPROM.read(1) == 0)
+  {
     EEPROM.write(1, 1);
     EEPROM.commit();
   }
 }
 
-void setupLoRa(){ 
+void setupLoRa()
+{ 
   SPI.begin(SCK, MISO, MOSI, SS);
   LoRa.setPins(SS, RST, DI00);
 
-  if (!LoRa.begin(BAND)){
+  if (!LoRa.begin(BAND))
+  {
     display.clear();
     display.setFont(ArialMT_Plain_24);
     display.drawString(0, 0, "ALERTA!");
@@ -100,7 +125,7 @@ void setupLoRa(){
 
     digitalWrite(PIN_LED_ERR, HIGH);
     
-    while (1);
+    while(1);
   }
 
   LoRa.enableCrc();
@@ -109,7 +134,39 @@ void setupLoRa(){
   digitalWrite(PIN_LED_OK, HIGH);
 }
 
-void setup() {
+void sendPacket(int floaterStatus)
+{
+  if (!LoRa.beginPacket())
+  {
+    display.clear();
+    display.drawString(0, 30, "LoRa não está pronto!");
+    display.display();
+
+    digitalWrite(PIN_LED_OK, LOW);
+    digitalWrite(PIN_LED_ERR, HIGH);
+    
+    while(1);
+  }
+  
+  LoRa.print("="); //delimitador
+  LoRa.print(floaterStatus);
+  LoRa.print("#"); //delimitador
+  
+  if (!LoRa.endPacket())
+  {
+    display.clear();
+    display.drawString(0, 30, "Erro ao enviar dados!");
+    display.display();
+
+    digitalWrite(PIN_LED_OK, LOW);
+    digitalWrite(PIN_LED_ERR, HIGH);
+
+    while(1);
+  }
+}
+
+void setup()
+{
   Serial.begin(9600);
 
   pinMode(PIN_MIN_LIMIT, INPUT);
@@ -126,7 +183,8 @@ void setup() {
   setupLoRa();
 }
 
-void loop() {
+void loop()
+{
   display.drawString(0, 0, "Monitorando...");
   display.display();
 
